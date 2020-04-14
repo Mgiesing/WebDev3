e4c4c5e8f4e4e0fcfa1aa72d160490fc32ced11e
Marco <marcogiesing02@gmail.com>
2020-04-14 22:19:19 +0200

    Added comments

--- input.php
+++ input.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-// When someone doesn't have a Id, there are send to the login.php page.
+// When someone doesn't have a Id, there are send to the login.php page. //Marco
 if (!isset($_SESSION['Id'])) {
     //TODO: Implemnt multile types of users: a.k.a. Student, Manager enz. and change the page accordingly.
     header('Location: login.php');
