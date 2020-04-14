da8e7328cb2851341f26c99480bd0bbb27fa41ea
Marco <marcogiesing02@gmail.com>
2020-04-14 17:33:10 +0200

    Small Changes

---
+++ naam.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Users(
+                      email VARCHAR(100) NOT NULL,
+                      password VARCHAR(15) NOT NULL,
+                      Docent tinyint(1) NOT NULL,
+                      code INT(11) NOT NULL AUTO_INCREMENT,
+                      PRIMARY KEY (code)
+)
