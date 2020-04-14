1db424638d0deb5df5c32d127016421e7c863f0c
Marco <marcogiesing02@gmail.com>
2020-04-14 22:21:43 +0200

    Update logoutListener.php

--- php/logoutListener.php
+++ php/logoutListener.php
@@ -2,6 +2,7 @@
 //Start user data storage (so we know who is logged in)
 //Put the session to null so it basically kills its self.
 //Then go back to index.
+//Marco
 session_start();
 $_SESSION = null;
 session_destroy();
