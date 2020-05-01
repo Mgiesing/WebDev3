9bab0e480e41be6040e52cafe8f76d9bd4911397
SnowPiece <54859642+SnowPiece@users.noreply.github.com>
2020-05-01 18:28:54 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -9,131 +9,143 @@ session_start();
 
 
 <html>
-  <head>
+<head>
     <title>Homepage</title>
     <meta charset="UTF-8" />
     <meta
-      name="viewport"
-      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
     />
     <link
-      rel="stylesheet"
-      href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-      integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
-      crossorigin="anonymous"
+        rel="stylesheet"
+        href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh"
+        crossorigin="anonymous"
     />
     <script
-      src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
-      integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
-      crossorigin="anonymous"
+        src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+        integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
+        crossorigin="anonymous"
     ></script>
     <script
-      src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
-      integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
-      crossorigin="anonymous"
+        src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"
     ></script>
     <script
-      src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
-      integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-      crossorigin="anonymous"
+        src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
+        integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
+        crossorigin="anonymous"
     ></script>
     <link rel="stylesheet" type="text/css" href="Style.css" />
-  </head>
+</head>
 
-  <body>
-    <div>
-      <nav
+<body>
+<div>
+    <nav
         class="navbar navbar-expand-sm navbar-dark"
         style="background-color: #000000;"
-      >
+    >
         <a
-          class="navbar-brand"
-          href="https://start.nhlstenden.com/"
-          target="blanc"
-          >Start NHL Stenden</a
+            class="navbar-brand"
+            href="https://start.nhlstenden.com/"
+            target="blanc"
+        >Start NHL Stenden</a
         >
         <button
-          class="navbar-toggler d-lg-none"
-          type="button"
-          data-toggle="collapse"
-          data-target="#collapsibleNavId"
-          aria-controls="collapsibleNavId"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+            class="navbar-toggler d-lg-none"
+            type="button"
+            data-toggle="collapse"
+            data-target="#collapsibleNavId"
+            aria-controls="collapsibleNavId"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
         ></button>
         <div class="collapse navbar-collapse" id="collapsibleNavId">
-          <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-              <?php
-              
-            //Dynamic buttons if user is not logged in show home if user is logged in show the rest aswell  //Marco
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <?php
 
-              if (!isset($_SESSION['username'])) {
-                  echo '<a class="nav-item active"><a class="nav-link" href="Index.php">Home</a>';
+                //Dynamic buttons if user is not logged in show home if user is logged in show the rest aswell  //Marco
 
-              }
+                //Dynamic buttons do work but since Ynte didn't finish the login system you can't physically login.
+                //The buttons DO work if you could login.
+                //This is the case with all the dynamic buttons because they look for a userId and if no one is logged in there is no userId
+                //If you would like to go to the pages you could currently without being logged in change the link from for example
+                //  /Index.php to Zoek.php
+                if (!isset($_SESSION['username'])) {
+                    echo '<a class="nav-item active"><a class="nav-link" href="index.php">Home</a>';
 
-              if (isset($_SESSION['username'])) {
-                  echo '<a class="nav-item active"><a class="nav-link" href="Index.php">Home</a>';
-                  echo '<a class="nav-item active"><a class="nav-link" href="input.php">Database</a>';
-                  echo '<a class="nav-item active"><a class="nav-link" href="Zoek.php">Zoek Bronnen</a>';
-                  echo '<a class="nav-item active"><a class="nav-link" href="#">Portfolio</a>';
-                  echo '<a style="color: white;" class="nav-link" href="php/logoutListener.php">Logout</a>';
+                }
 
-              }
-              ?>
-          </ul>
+                if (isset($_SESSION['username'])) {
+                    echo '<a class="nav-item active"><a class="nav-link" href="input.php">Database</a>';
+                    echo '<a class="nav-item active"><a class="nav-link" href="Zoek.php">Zoek Bronnen</a>';
+                    echo '<a class="nav-item active"><a class="nav-link" href="#">Portfolio</a>';
+                    echo '<a style="color: white;" class="nav-link" href="logoutListener.php">Logout</a>';
+
+                }
+                ?>
+            </ul>
 
 
-          <form class="form-inline my-2 my-lg-0">
-            <input
-              class="form-control mr-sm-2"
-              type="text"
-              placeholder="Zoeken"
-            />
-            <button class="btn btn-outline-light my-2 my-sm-0" type="submit">
-              Zoeken
-            </button>
-          </form>
+            <form class="form-inline my-2 my-lg-0" method="post" action="Zoek.php">
+                <input
+                    class="form-control mr-sm-2" type="text" placeholder="Zoeken" name="zoek"
+                />
+                <button class="btn btn-outline-light my-2 my-sm-0" type="submit" name="Noten">
+                    Zoeken
+                </button>
+            </form>
         </div>
-      </nav>
-    </div>
-    <div class="homepage">
-      <div class="container">
+    </nav>
+</div>
+<div class="homepage">
+    <div class="container">
         <div class="row">
-          <div class="col-sm-3"></div>
-          <div class="col-xl-2">
-              <button class="btn btn-homepage" id="btn1" href="">Aard, nut en noodzaak van natuurwetenschappen</button><br>
-              <button class="btn btn-homepage" id="btn2" href="">Begripsontwikkeling</button><br>
-              <button class="btn btn-homepage" id="btn3" href="">Practicum didactiek</button><br>
-          </div>
-          <div class="col-xl-2">
-            <button class="btn btn-homepage" id="btn4" href="">Leerpocessen, lesopbouw en toetsing</button><br>
-              <button class="btn btn-homepage" id="btn5" href="">Natuurweten-schappelijke denk- en werkwijzen</button><br>
-              <button class="btn btn-homepage" id="btn6" href="">Digitale didactiek</button><br>
+            <div class="col-sm-3"></div>
+            <div class="col-xl-2">
+                <form method="post" action="Zoek.php">
+                    <button class="btn btn-homepage" type="submit" name="Noten" id="btn1">Aard, nut en noodzaak van natuurwetenschappen</button><br>
+                </form>
+                <form method="post" action="Zoek.php">
+                <button class="btn btn-homepage" type="submit" name="Youtuber" id="btn2" href="">Begripsontwikkeling</button><br>
+                </form>
+                <form method="post" action="Zoek.php">
+                <button class="btn btn-homepage" id="btn3" type="submit" name="Games" href="">Practicum didactiek</button><br>
+                </form>
+            </div>
+            <div class="col-xl-2">
+                <form method="post" action="Zoek.php">
+                <button class="btn btn-homepage" id="btn4" type="submit" name="Games">Leerpocessen, lesopbouw en toetsing</button><br>
+                </form>
+                <button class="btn btn-homepage" id="btn5" href="">Natuurweten-schappelijke denk- en werkwijzen</button><br>
+                <button class="btn btn-homepage" id="btn6" href="">Digitale didactiek</button><br>
 
-              <!-- Dynamic buttons -->
-              <?php
-              //Dynamic buttons if user is not logged in show login if user is logged in show logout  //Marco
-              if (!isset($_SESSION['username'])) {
-              echo '<button class="nav-item active"><a class="nav-link" href="login.php">Inloggen</a></button>';
+                <!-- Dynamic buttons -->
+                <?php
+                //Dynamic buttons if user is not logged in show login if user is logged in show logout  //Marco
+                if (!isset($_SESSION['username'])) {
+                    echo '<button class="nav-item active"><a class="nav-link" href="login.php">Inloggen</a></button>';
 
-              }
-              if (isset($_SESSION['username'])) {
-                  echo '<a style="color: white;" class="nav-link" href="php/logoutListener.php">Logout</a>';
-              }
-              ?>
-          </div>
+                }
+                if (isset($_SESSION['username'])) {
+                    echo '<a style="color: white;" class="nav-link" href="logoutListener.php">Logout</a>';
 
-          <div class="col-xl-2">
-              <button class="btn btn-homepage" id="btn7" href="">Doorlopende leerlijn en samenhang met andere vakken</button><br>
-              <button class="btn btn-homepage" id="btn8" href="">Social scientific issues<br>
-              <button class="btn btn-homepage" id="btn9" href="">Vakdidactische persoonlijke professionele ontwikkeling<br>
-          </div>
-          <div class="col-sm-3">
-            <button class="btn btncontact">Contact</button>
-          </div>
-      </div>
+
+                }
+                ?>
+            </div>
+
+            <div class="col-xl-2">
+                <button class="btn btn-homepage" id="btn7" href="">Doorlopende leerlijn en samenhang met andere vakken</button><br>
+                <button class="btn btn-homepage" id="btn8" href="">Social scientific issues<br>
+                    <button class="btn btn-homepage" id="btn9" href="">Vakdidactische persoonlijke professionele ontwikkeling<br>
+            </div>
+            <div class="col-sm-3">
+                <button class="btn btncontact">Contact</button>
+            </div>
+        </div>
     </div>
-    </div>
-  </body>
+</div>
+</body>
 </html>
