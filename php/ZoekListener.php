af777316723381833d3dc06908b68e5cd2adc9a4
SnowPiece <54859642+SnowPiece@users.noreply.github.com>
2020-05-01 18:32:41 +0200

    Create ZoekListener.php

---
+++ php/ZoekListener.php
@@ -0,0 +1,51 @@
+<?php
+
+require 'Zoekfunction.php';
+
+
+
+
+
+if(isset($_POST["zoek"])) {
+    if (empty($_POST['zoek'])) {
+
+        TextBox();
+
+    } else {
+
+        TextBoxZoek();
+
+    }
+}
+
+
+
+    if(isset($_POST['checkbox']) || isset($_POST["Noten"]) || isset($_POST["Youtuber"]) || isset($_POST["Games"]) || isset($_POST["Memes"])) {
+
+
+        if (isset($_POST["Noten"])) {
+
+            TextBoxNoten();
+        }
+
+        if (isset($_POST["Youtuber"])) {
+
+            TextBoxYoutuber();
+        }
+
+        if (isset($_POST["Games"])) {
+
+            TextBoxGames();
+
+        }
+
+        if (isset($_POST["Memes"])) {
+
+            TextBoxMemes();
+
+        } else if (!isset($_POST['Noten']) && !isset($_POST['Youtuber']) && !isset($_POST['Games']) && !isset($_POST['Memes'])) {
+
+            TextBox();
+
+        }
+    }
