5ff09e6df5c358391aaf3ecdfdcd7983c7982f56
SnowPiece <54859642+SnowPiece@users.noreply.github.com>
2020-05-13 21:16:31 +0200

    Update inputListener.php

--- php/inputListener.php
+++ php/inputListener.php
@@ -1,11 +1,6 @@
 <?php
 
 require_once 'inputfunction.php';
-// When someone doesn't have a Id, there are send to the login.php page. //Marco
-if (isset($_SESSION['Id'])) {
-    //TODO: Implemnt multile types of users: a.k.a. Student, Manager enz. and change the page accordingly.
-    header('Location: login.php');
-}
 
 $conn = connectdb();
 
