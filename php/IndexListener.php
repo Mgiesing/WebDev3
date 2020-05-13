8496111f0fc432fe1cce942113a11a5bf69ea211
SnowPiece <54859642+SnowPiece@users.noreply.github.com>
2020-05-13 21:25:48 +0200

    Update IndexListener.php

--- php/IndexListener.php
+++ php/IndexListener.php
@@ -1,20 +1,20 @@
 <?php
 function ShowNavbar(){
     if (!isset($_SESSION['username'])) {
-        echo '<a class="nav-item active"><a class="nav-link" href="index.php">Home</a>';
+        echo '<a class="nav-item active"><a class="nav-link" href="Index.php">Home</a>';
 
     }
 
     if(isset($_SESSION['username'])) {
         if ($_SESSION['Docent'] == !true) {
-            echo '<a class="nav-item active"><a class="nav-link" href="index.php">Home</a>';
+            echo '<a class="nav-item active"><a class="nav-link" href="Index.php">Home</a>';
             echo '<a class="nav-item active"><a class="nav-link" href="Zoek.php">Zoek Bronnen</a>';
             echo '<a class="nav-item active"><a class="nav-link" href="#">Portfolio</a>';
             echo '<a style="color: white;" class="nav-link" href="logoutListener.php">Logout</a>';
         }
 
         if ($_SESSION['Docent'] == true) {
-            echo '<a class="nav-item active"><a class="nav-link" href="index.php">Home</a>';
+            echo '<a class="nav-item active"><a class="nav-link" href="Index.php">Home</a>';
             echo '<a class="nav-item active"><a class="nav-link" href="input.php">Database</a>';
             echo '<a class="nav-item active"><a class="nav-link" href="Zoek.php">Zoek Bronnen</a>';
             echo '<a class="nav-item active"><a class="nav-link" href="#">Portfolio</a>';
